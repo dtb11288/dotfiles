01bdbefbb4e2a018aeb8e30ab970060fc44cd3be
Binh <dau.thanh.binh@gmail.com>
2016-01-20 08:33:35 +0700

    update

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
@@ -8,28 +9,63 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
--- NOTES: 0.10 works much better than 0.9, unfortunately distros mostly package 0.9 atm
--- xmobar and fullscreen flash vids (youtube): http://code.google.com/p/xmobar/issues/detail?id=41
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
--- TODO: would still like fullscreen flash vids to not crop and leave xmobar drawn
--- TODO: remove the red border when doing fullscreen? tried adding 'smartBorders' to the layoutHook but that didn't work
--- TODO: hook in TopicSpaces, start specific apps on specific workspaces
+-- colors
+colorFocusedBorder = "#8787af"
+colorNormalBorder = "#202020"
+colorXmobarCurrent = "#87afd7"
+colorXmobarTitle = "#ffaf5f"
+colorXmobarUrgent = "red"
+colorFallback = ""
 
+-- define default config
+baseConfig = desktopConfig
+
+-- main
 main = do
-    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
-    xmonad defaultConfig {
-        modMask = mod4Mask,
-        terminal = "sakura -e tmux",
-        -- if you are using xmonad 0.9, you can avoid web flash videos getting cropped in fullscreen like so:
-        -- manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig,
-        -- (no longer needed in 0.10)
-        manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts $ layoutHook defaultConfig,
-        focusedBorderColor = "#6A555C", --"#404752"
-        normalBorderColor = "#404752", --"#343C48"
-        logHook = dynamicLogWithPP $ xmobarPP
-            {
-                ppOutput = hPutStrLn xmproc,
-                ppTitle = xmobarColor "green" "" . shorten 50
-            }
+    xmtray <- spawnPipe myTrayBar
+    xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- my config
+myConfig = baseConfig
+    { modMask = myModMask
+    , terminal = myTerminal
+    , workspaces = myWorkspaces
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , focusedBorderColor = colorFocusedBorder
+    , normalBorderColor = colorNormalBorder
+    , borderWidth = myBorderWidth
     }
+
+-- my PP
+myPP = xmobarPP
+    { ppCurrent = xmobarColor colorXmobarCurrent colorFallback . wrap "[" "]"
+    , ppTitle = xmobarColor colorXmobarTitle colorFallback
+    , ppUrgent = xmobarColor colorXmobarUrgent colorXmobarUrgent
+    }
+
+-- status bar
+myBar = "xmobar ~/.xmonad/xmobar.hs"
+
+-- keys
+toggleStrutsKey XConfig { XMonad.modMask = myModMask } = (myModMask, xK_b)
+
+-- use super key
+myModMask = mod4Mask
+
+-- terminal
+myTerminal = "urxvt -e tmux"
+
+-- workspaces
+myWorkspaces = map show [1..9]
+
+-- tray bar
+myTrayBar = "killall -9 trayer; trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 8 --height 22 --transparent true --tint 0x000000"
+
+-- border width
+myBorderWidth = 1
+
+-- my keys
