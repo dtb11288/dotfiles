997d086f6373ed6181af6682b0320266b180ed08
Binh <dau.thanh.binh@gmail.com>
2016-01-23 21:24:13 +0700

    update

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -26,24 +26,26 @@ baseConfig = desktopConfig
 
 -- main
 main = do
-    xmtray <- spawnPipe myTrayBar
+    spawnPipe myTrayBar
     xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
 -- my config
-myConfig = baseConfig {
-      modMask = myModMask
+myConfig = baseConfig
+    { modMask = mod4Mask
     , terminal = myTerminal
+    , focusFollowsMouse = False
     , workspaces = myWorkspaces
     , manageHook = myManageHook
     , layoutHook = myLayoutHook
     , focusedBorderColor = colorFocusedBorder
     , normalBorderColor = colorNormalBorder
     , borderWidth = myBorderWidth
-    } `additionalKeys` myKeys
+    , keys = \c -> myKeys c `M.union` keys defaultConfig c
+    }
 
 -- manage apps
-myManageHook = manageDocks <+> manageHook baseConfig <+> composeOne [
-      isFullscreen -?> (doF W.focusDown <+> doFullFloat)
+myManageHook = manageDocks <+> manageHook baseConfig <+> composeOne
+    [ isFullscreen -?> (doF W.focusDown <+> doFullFloat)
     , resource =? "trayer" -?> doIgnore
     ]
 
@@ -51,41 +53,68 @@ myManageHook = manageDocks <+> manageHook baseConfig <+> composeOne [
 myLayoutHook = avoidStruts $ layoutHook baseConfig
 
 -- my PP
-myPP = xmobarPP {
-      ppCurrent = xmobarColor colorXmobarCurrent colorFallback . wrap "[" "]"
-    , ppTitle = xmobarColor colorXmobarTitle colorFallback
+myPP = xmobarPP
+    { ppCurrent = xmobarColor colorXmobarCurrent colorFallback . wrap "[" "]"
+    , ppTitle = xmobarColor colorXmobarTitle colorFallback . shorten 50
     , ppUrgent = xmobarColor colorXmobarUrgent colorXmobarUrgent
     }
 
 -- my keys
-myKeys = [
+myKeys conf@XConfig {XMonad.modMask = modMask} = M.fromList
     -- volumn keys
-      ((0, xF86XK_AudioLowerVolume   ), spawn "amixer -q sset Master 5%-")
-    , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer -q sset Master 5%+ unmute")
-    , ((0, xF86XK_AudioMute          ), spawn "amixer -q sset Master toggle")
+    [ ((0, xF86XK_AudioLowerVolume  ), spawn "amixer -q sset Master 5%-")
+    , ((0, xF86XK_AudioRaiseVolume  ), spawn "amixer -q sset Master 5%+ unmute")
+    , ((0, xF86XK_AudioMute         ), spawn "amixer -q sset Master toggle")
 
     -- brightness keys
-    , ((0, xF86XK_MonBrightnessUp    ), spawn "xbacklight -inc 5 # increase screen brightness")
-    , ((0, xF86XK_MonBrightnessDown  ), spawn "xbacklight -dec 5 # decrease screen brightness")
+    , ((0, xF86XK_MonBrightnessUp   ), spawn "xbacklight -inc 5 # increase screen brightness")
+    , ((0, xF86XK_MonBrightnessDown ), spawn "xbacklight -dec 5 # decrease screen brightness")
+
+    -- dmenu
+    , ((modMask, xK_p               ), spawn myDmenu)
     ]
 
--- status bar
-myBar = "xmobar ~/.xmonad/xmobar.hs"
+myDmenu = unwords
+    [ "dmenu_run"
+    , "-fn", "-*-roboto-medium-*-*-*-*-*-*-*-*-*-*-*"
+    ]
 
 -- show/hide top bar
-toggleStrutsKey XConfig { XMonad.modMask = myModMask } = ( myModMask, xK_f )
-
--- use super key
-myModMask = mod4Mask
+toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_f)
 
 -- terminal
-myTerminal = "urxvt -e tmux"
+myTerminal = unwords
+    [ "urxvt"
+    , "-e", "tmux"
+    ]
 
 -- workspaces
 myWorkspaces = map show [1..9]
 
 -- tray bar
-myTrayBar = "killall -9 trayer; trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 5 --height 23 --transparent true --alpha 0 --tint 0x080808"
+myTrayBar = unwords
+    -- kill trayer
+    [ "killall"
+    , "-9", "trayer;"
+    -- then re-run it
+    , "trayer"
+    , " --edge", "top"
+    , "--align", "right"
+    , "--SetDockType", "true"
+    , "--SetPartialStrut", "true"
+    , "--expand", "true"
+    , "--width", "5"
+    , "--height", "23"
+    , "--transparent", "true"
+    , "--alpha", "0"
+    , "--tint", "0x080808"
+    ]
+
+-- status bar
+myBar = unwords
+    [ "xmobar"
+    , "~/.xmonad/xmobar.hs"
+    ]
 
 -- border width
 myBorderWidth = 1
