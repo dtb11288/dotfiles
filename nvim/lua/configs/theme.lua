3c3abe2522e907783cf4ecd34e3eb24431b02dd3
Binh Dau <dau.thanh.binh@gmail.com>
2023-03-08 17:12:31 +0700

    background color for float

--- nvim/lua/configs/theme.lua
+++ nvim/lua/configs/theme.lua
@@ -1,7 +1,7 @@
 vim.opt.background = 'dark'
 vim.opt.showmode = false
 vim.g.zenbones_compat = 1
-vim.cmd[[colorscheme zenbones]]
+vim.cmd [[colorscheme zenbones]]
 require('nvim-web-devicons').setup()
 require('lualine').setup({
   options = {
@@ -11,3 +11,5 @@ require('lualine').setup({
     lualine_c = { require('auto-session-library').current_session_name }
   }
 })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'black', fg = 'black' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'black' })
