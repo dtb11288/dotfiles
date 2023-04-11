104eb8900d24a4346b9e8c9657219b66ef9bb34e
binh <dau.thanh.binh@gmail.com>
2023-04-11 11:13:44 +0700

    some update for image preview

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -75,7 +75,7 @@ case "$mimetype" in
         try highlight --out-format=ansi "$path" && { dump | trim; exit 5; } || exit 2;;
     # Ascii-previews of images:
     image/*)
-        img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
+        chafa -c 256 -s "$widthx$height" "$path" && exit 4 || exit 1;;
     # Image preview for videos, disabled by default:
     # video/*)
     #     ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
