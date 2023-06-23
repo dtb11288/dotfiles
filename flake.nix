5307fc31e5ac67abe361ee139069e8cf8cbc08e1
Binh Dau <dau.thanh.binh@gmail.com>
2023-06-23 23:59:51 +0700

    add flake config

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  # Override nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil
+        ];
+      };
+    });
+}
