fb05bc6de0c4fdd7930e8fe2291ac67834de346b
Binh Dau <dau.thanh.binh@gmail.com>
2023-06-24 11:50:16 +0700

    remove file

--- nixos/system/xps15.nix
+++ nixos/system/xps15.nix
@@ -1,4 +1,14 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimu=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in
+{
   imports = [
     ./configuration.nix
 
@@ -6,4 +16,17 @@
     ./hardware-xps15.nix
     ./nvidia-xps15.nix
   ];
+
+  environment.systemPackages = [ nvidia-offload ];
+
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+  hardware.nvidia.prime = {
+    offload.enable = true;
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
+  };
 }
